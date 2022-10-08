799f4f213d46e752c494a0e535c12bd23ea2bd33
AfanKulaglic <afankul42@gmail.com>
2022-10-08 14:48:53 +0200

    my commit

--- components/ArticleItem/InfoArticle.jsx
+++ components/ArticleItem/InfoArticle.jsx
@@ -2,7 +2,6 @@ import React,{useState}from 'react'
 import { Button  } from 'react-bootstrap'
 import shopIcon from '../../imgs/shopIcon.png'
 import orderIcon from '../../imgs/orderIcon.png'
-import Image from 'next/Image'
 import { IconButton } from '@mui/material'
 import { useEffect } from 'react'
 import Router from 'next/router'
@@ -25,13 +24,13 @@ export const InfoArticle = ({price,text,name}) => {
         <Button value='naruci' href='/Orders'>
             <div style={{display:'flex',justifyContent:'center',padding:'1px'}}>
                 <p style={{marginTop:'2vh',marginRight:'3px'}}>Naruci</p>
-                <IconButton href='/Orders'><Image src={orderIcon} height='22' width='22'/></IconButton>
+                <IconButton href='/Orders'><img src={orderIcon} height='22' width='22'/></IconButton>
             </div>
         </Button > 
         <Button style={{marginLeft:'7px',backgroundColor:'green'}}>
             <div style={{display:'flex',justifyContent:'center',padding:'1px'}}>
                 {nameW && <p style={{marginTop:'2vh',marginRight:'3px'}} onClick={() => {Router.push({ pathname: "/Cart", query: { name: Router.query.name } },'/Korpa')}}>{nameW}, stavi u korpu</p>}
-                <IconButton src='/Orders' ><Image src={shopIcon} height='22' width='22'/></IconButton>
+                <IconButton src='/Orders' ><img src={shopIcon} height='22' width='22'/></IconButton>
             </div>
         </Button> 
     </div>
